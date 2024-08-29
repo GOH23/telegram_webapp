c271de635e06dd5dea02a85dcceb5397529aecb2
GOH23 <padadamlpd@gmail.com>
2024-08-29 15:32:25 +0300

    hotfix5

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,6 @@ import { ProfilePage } from "@/app/ui/pages/profile_page/profile_page";
 import { useEffect } from "react";
 
 export default function Home() {
-    var app: WebApp | undefined
-    useEffect(()=>{
-        app = window.Telegram?.WebApp
-    },[])
-    return (<ProfilePage TelegramUserData={app}/>);
+
+    return (<ProfilePage/>);
 }
--- src/app/ui/pages/profile_page/profile_page.tsx
+++ src/app/ui/pages/profile_page/profile_page.tsx
@@ -1,4 +1,10 @@
-export function ProfilePage({TelegramUserData}: {TelegramUserData:  WebApp | undefined}){
+import { useEffect } from "react"
+
+export function ProfilePage(){
+    var app: WebApp | undefined
+    useEffect(()=>{
+        app = window.Telegram?.WebApp
+    },[])
     return <main className="min-h-dvh">
         <div className={'bg-stone-900 h-16 mx-2 mt-2 rounded-2xl p-2'}></div>
         <div className={'bg-stone-900 h-16 mx-2 mt-2 rounded-2xl p-2'}></div>
