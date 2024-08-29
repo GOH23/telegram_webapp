025789d9512f7beace6c496783299d9233bc60d0
GOH23 <padadamlpd@gmail.com>
2024-08-29 15:23:51 +0300

    hotfix4 [added UseEffect to profile page]

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,9 @@ import { ProfilePage } from "@/app/ui/pages/profile_page/profile_page";
 import { useEffect } from "react";
 
 export default function Home() {
-    var app: WebApp | undefined = window.Telegram?.WebApp
+    var app: WebApp | undefined
+    useEffect(()=>{
+        app = window.Telegram?.WebApp
+    },[])
     return (<ProfilePage TelegramUserData={app}/>);
 }
