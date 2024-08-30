b9aaab59b6d927f14560956bde5dd4c9a3f6f859
GOH23 <padadamlpd@gmail.com>
2024-08-30 11:58:24 +0300

    updated

--- src/app/ui/pages/main_page/main_page.tsx
+++ src/app/ui/pages/main_page/main_page.tsx
@@ -52,7 +52,7 @@ export default function MainPage() {
         <main className="min-h-dvh">
             <Filter_game />
             <p>
-              {initDataRaw}  
+
             </p>
             <div className="flex flex-row pt-10 flex-wrap items-stretch justify-center  md:gap-5 gap-2">
                 {CardData.map((el, ind) => <Card {...el} key={ind} value={Number(el.value)} />)}
