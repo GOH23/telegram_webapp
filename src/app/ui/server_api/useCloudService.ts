0aef81707f960cd3beebf3b66522e6f5521e0c4f
GOH23 <padadamlpd@gmail.com>
2024-09-09 22:08:15 +0300

    toke

--- src/app/ui/server_api/useCloudService.ts
+++ src/app/ui/server_api/useCloudService.ts
@@ -3,11 +3,13 @@ import { useState } from "react"
 
 const useCloudService = (web_app: WebApp,nameVal: string) =>{
     const [cloud,setcloud] = useState("")
+
     try{
         web_app.CloudStorage.getItem(nameVal,(_,value)=> setcloud(value!))
     }catch{
 
     }
+    web_app.showAlert(cloud)
     return cloud
 }
 export {useCloudService}
