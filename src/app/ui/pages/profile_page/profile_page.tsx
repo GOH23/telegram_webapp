8107072d9e45435224ad8c8d0006295f3a858eeb
GOH23 <padadamlpd@gmail.com>
2024-09-11 19:48:50 +0300

    HotFix Added UserData

--- src/app/ui/pages/profile_page/profile_page.tsx
+++ src/app/ui/pages/profile_page/profile_page.tsx
@@ -12,15 +12,19 @@ export function ProfilePage() {
     const { web_app, login_data, loadingState } = useConfig()
 
     return <main className={'min-h-dvh text-white'}>
+        <div className={'bg-stone-900 h-auto mx-2 mt-2 rounded-2xl p-2 opacity-60 blur-lg'}>
+            <p >Ваши данные</p>
+            <p >Ваш никнейм:</p>
+            <p >{login_data.user.username}</p>
+            <p >Ваш роль:</p>
+            <p >{login_data.user.roles}</p>
+            <Link href={'/admin'}></Link>
+        </div>
         <DataCard ImageSource={"/logos/gi_logo.png"} GameName={"Genshin Impact"} />
         <DataCard ImageSource={"/logos/hsr_logo.png"} GameName={"Honkai Star Rail"} />
         {login_data.user.roles == "Admin" && <a href={"https://t.me/sadovolk_store"} className={
             clsx('bg-cyan-600 text-lg p-1 mx-2 sm:block m-2 rounded-md hover:bg-cyan-700 transition-all duration-1000 text-white')
         } type="blank">Наш телеграм</a>}
-        <div className={'bg-stone-900 h-32 mx-2 mt-2 rounded-2xl p-2'}>
-            <p >Ваши данные</p>
-            <p >{login_data.user.username}</p>
-            <Link href={'/admin'}></Link>
-        </div>
+
     </main>
 }
