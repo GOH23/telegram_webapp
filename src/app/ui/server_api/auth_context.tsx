b58ec2093dc16edad2dad2bdbd96c996686c2e6c
GOH23 <padadamlpd@gmail.com>
2024-09-14 20:36:00 +0300

    fixed initdata

--- src/app/ui/server_api/auth_context.tsx
+++ src/app/ui/server_api/auth_context.tsx
@@ -16,7 +16,7 @@ export default function AuthProvider({
 }>) {
     
     //const { data, trigger } = useSWRMutation('/auth/login', PostFetcher)
-    const { data, isLoading, error } = useSWR(get_URL('/auth/login'), (url) => fetcher(url, "query_id=AAFbEU9XAAAAAFsRT1cM6QYY&user=%7B%22id%22%3A1464799579%2C%22first_name%22%3A%22Daniil%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22goh222%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1726310741&hash=6da0d4232a521444fcf12b81199e2b69abfd5195d90672a36f141c5cd589b2e2"))
+    const { data, isLoading, error } = useSWR(get_URL('/auth/login'), (url) => fetcher(url,tg.initData))
     if (isLoading) {
         return (<LoadingPage/>)
     }
